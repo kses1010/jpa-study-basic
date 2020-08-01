71e54c2b346442ed63333eda3eb9aaf71f97c00c
Sunny <thstkd92@gmail.com>
2020-08-02 01:37:01 +0900

    실전 예제6 - 값 타입 매핑

--- src/main/java/hello/jpa/domain/Member.java
+++ src/main/java/hello/jpa/domain/Member.java
@@ -13,14 +13,22 @@ public class Member extends BaseEntity {
     private Long id;
 
     private String name;
-    private String city;
-    private String street;
-    private String zipcode;
+
+    @Embedded
+    private Address address;
 
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
     public Member() {}
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 
     public Long getId() {
         return id;
@@ -38,30 +46,6 @@ public class Member extends BaseEntity {
         this.name = name;
     }
 
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
     public List<Order> getOrders() {
         return orders;
     }
@@ -69,4 +53,5 @@ public class Member extends BaseEntity {
     public void setOrders(List<Order> orders) {
         this.orders = orders;
     }
+
 }
