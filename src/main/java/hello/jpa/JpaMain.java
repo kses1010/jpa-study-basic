0021a2da3c9034a148f6a8c7cc91eb8999e1b93d
Sunny <thstkd92@gmail.com>
2020-08-02 12:27:29 +0900

    JPA nativeQuery 기본 예제

--- src/main/java/hello/jpa/JpaMain.java
+++ src/main/java/hello/jpa/JpaMain.java
@@ -4,10 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -18,19 +14,7 @@ public class JpaMain {
         // code
         try {
 
-            // Criteria 사용 준비
-            CriteriaBuilder cb = em.getCriteriaBuilder();
-            CriteriaQuery<Member> query = cb.createQuery(Member.class);
-
-            Root<Member> m = query.from(Member.class);
-
-            CriteriaQuery<Member> cq = query.select(m);
-            String username = "abcde";
-            if (username != null) {
-                cq = query.select(m).where(cb.equal(m.get("name"), "kim"));
-            }
-
-            List<Member> resultList = em.createQuery(cq)
+            em.createNativeQuery("select member_id, city, street, name from member")
                     .getResultList();
 
             tx.commit();
